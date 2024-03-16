de3f965208617f92025ded36604533383745cae8
KateYankovenko <k.yankovenko@ukr.net>
2024-03-16 20:09:37 +0100

    2.3

--- level2.3.js
+++ level2.3.js
@@ -30,7 +30,17 @@ console.log(toCheckLet("Work", "Weather"));
 
 // A certain string is given. Find the position of the third zero in the line.
 
+const toFindPos = str => { 
+    let arr = str.split(" ");
+   
+    for (let i = 0; i <= arr.length; i++){
+        if(arr[i] === "0") {
+            return i;
+        }
+    }
+};
 
+console.log(toFindPos("Zero is 0 here 0, find 0 it 0!"));
 
 
 // No. 3
@@ -40,7 +50,16 @@ console.log(toCheckLet("Work", "Weather"));
 // '12,34,56'
 // Find the sum of these numbers.
 
+const toFindSum = (...argv) => { 
+    let sum = 0;
+    
+    for (let i = 0; i < argv.length; i++) {
+        sum += argv[i];
+    }
+    return sum;
+};
 
+console.log(toFindSum(12, 34, 56));
 
 // No. 4
 
@@ -48,9 +67,27 @@ console.log(toCheckLet("Work", "Weather"));
 
 // '2025-12-31'
 // Convert this date to the following object:
-
 // {
 // 	year: '2025',
 // 	month: '12',
 // 	day: '31',
 // }
+
+
+// ----------------dateString.split('-') splits the date string into an array 
+// ----------------of substrings using "-" as the delimiter.
+// ----------------The destructuring assignment[year, month, day] assigns each 
+// ----------------part of the array to corresponding variables.
+
+const toObject = (strDate) => { 
+   const [year, month, day] = strDate.split('-');
+    // console.log(strDate.split('-'));//['2025', '12', '31']
+    const objDate = {
+        year: year,
+        month: month,
+        day: day
+    };
+    return objDate;
+};
+
+console.log(toObject("2025-12-31"));//{year: '2025', month: '12', day: '31'}
