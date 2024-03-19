f7a5f1fff22d2e98f30a340ba38c5bddc1163165
KateYankovenko <k.yankovenko@ukr.net>
2024-03-19 18:28:19 +0100

    2.6

--- level2.6.js
+++ level2.6.js
@@ -48,14 +48,14 @@ console.log(toNumRev(123, 456, 789));
 // '1 234 567'
 
 const toDigSep = str => { 
-    let newStr = "";
-    for (let i = 0; i < str.length; i++){
-        
+    let digits = str.split("");
+    for (let i = digits.length - 3; i > 0; i -= 3){//iterate backward from the third-to-last digit towards the beginning of the array.
+        digits.splice(i, 0, " "); // Insert a space character
     }
-    return newStr;
+    return digits.join("");
 };
 
-console.log(toDigSep("1234567"));
+console.log(toDigSep("1234567")); 
 
 // No. 4
 
@@ -87,11 +87,42 @@ console.log(toChangeCase('AbCdE'));
 // [12, 34, 56]
 
 
+const toMergePair = (...argv) => { 
+    let mergedArr = [];
+    
+    for (let i = 0; i < argv.length; i += 2){
+       mergedArr.push(argv[i] * 10 + argv[i + 1]);// Merge pairs of elements by summing them together
+    }
+    return mergedArr;
+};
+
+console.log(toMergePair(1, 2, 3, 4, 5, 6));
+
+
 
 // No. 6
 
 // Given a line with the words:
 // 'aaa bbb ccc eee fff'
-// Capitalize the first character of every second word on this line. 
+// Capitalize the first character of every second word on this line.
 // In our case it should look like this:
 // 'aaa Bbb ccc Eee fff'
+
+const toCahrCapitalize = str => { 
+    let capStr = "";
+ let words = str.split(" ");
+    for (let i = 0; i < words.length; i++){
+        const word = words[i];
+        if (i % 2 !== 0) { // Check if it's every second word
+            capStr += word[0].toUpperCase() + word.slice(1);//word.slice(1) is a string method that returns a portion of the string starting from the specified index (1 in this case) to the end of the string
+        } else {
+            capStr += word;
+        }
+        if (i !== words.length - 1) { // Add space between words, except for the last one
+            capStr += " ";
+        }
+    }
+    return capStr;
+};
+
+console.log(toCahrCapitalize('aaa bbb ccc eee fff'));
