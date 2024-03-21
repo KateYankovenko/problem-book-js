0facfff4af674d681ff71e456a3bd24420a1e11b
KateYankovenko <k.yankovenko@ukr.net>
2024-03-21 17:32:38 +0100

    3.2

--- level3.2.js
+++ level3.2.js
@@ -1,10 +1,39 @@
 // No. 1
 
-// Print to the console all numbers in the range from 10to 1000, whose penultimate digit is even.
+// Print to the console all numbers in the range from 10to 1000,
+//whose penultimate digit is even.
+
+const printEven = (min, max) => { 
+    let evenNums = [];
+    for (let i = min; i <= max; i++){
+        //each i num to string
+        const numStr = i.toString();
+        // Get the penultimate digit
+        const penultimateDigit = numStr[numStr.length - 2];
+
+        if (penultimateDigit %2 === 0) {
+            evenNums.push(i);
+        }
+    }
+    return evenNums;
+};
+
+console.log(printEven(10, 1000));
 
 // No. 2
 
 // Given an array. Remove every fifth element from it.
+
+const removeFifthEl = (...argv) => { 
+    let newArr = [...argv];// Create a copy of the array using the spread operator
+    for (let i = 4; i < newArr.length; i+=5){//To remove every fifth element from the array, the loop should indeed start from index 4 (the fifth element) and increment by 5 in each iteration
+        newArr.splice(i, 1);
+        i--;// Decrement i to account for the removed element
+    }
+    return newArr;
+};
+
+console.log(removeFifthEl(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5));//[1, 2, 3, 4, 6, 7, 8, 9, 1, 2, 3, 4]
 
 // No. 3
 
@@ -14,7 +43,15 @@
 //we get the following line:
 // '00000'
 
+const putZeroes = num => { 
+    let str = "";
+    for (let i = 0; i < num; i++){
+        str += "0";
+    }
+    return str;
+};
 
+console.log(putZeroes(5));
 
 // No. 4
 
@@ -23,6 +60,18 @@
 // Remove every second word from this line. In our case it should look like this:
 // 'aaa ccc fff'
 
+const removeSecond = str => { 
+    let newStr = str.split(" ");
+    let result = [];
+    for (let i = 0; i < newStr.length; i++) {
+        if (i % 2 === 0) {
+            result.push(newStr[i]);
+        }
+    }
+    return result.join(" ");
+};
+
+console.log(removeSecond('aaa bbb ccc eee fff'));
 
 // No. 5
 
