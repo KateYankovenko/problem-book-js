75095a246428438c10a1587a580f765cfb096a6b
KateYankovenko <k.yankovenko@ukr.net>
2024-03-24 19:23:25 +0100

    3.7

--- level3.7.js
+++ level3.7.js
@@ -2,13 +2,59 @@
 
 // Given a line with words. Sort the words in alphabetical order.
 
+const sortLine = str => { 
+    let arr = str.split(" ");
+    return [...arr].sort((a, b) => a.localeCompare(b));
+};
+
+console.log(sortLine("never come back"));
+
 // No. 2
 
 // A number is given. Get an array of divisors of this number.
 
+const getDivisirs = num => { 
+    const divisors = [];
+
+    // Iterate from 1 up to the given number
+    for (let i = 1; i <= num; i++) {
+        // If i divides num evenly, it's a divisor
+        if (num % i === 0) {
+            divisors.push(i); // Add i to the divisors array
+        }
+    }
+
+    return divisors; // Return the array of divisors
+};
+
+console.log(getDivisirs(234));
+
 // No. 3
 
 // Two numbers are given. Get an array of common divisors of these numbers.
+
+const getComDivisirs = (num1, num2) => { 
+    const divisors = [];
+
+    // Iterate from 1 up to the given number
+    for (let i = 1; i <= num1; i++) {
+        // If i divides num evenly, it's a divisor
+        if (num1 % i === 0) {
+            divisors.push(i); // Add i to the divisors array
+        }
+    }
+        for (let i = 1; i <= num2; i++) {
+        // If i divides num evenly, it's a divisor
+        if (num2 % i === 0) {
+            divisors.push(i); // Add i to the divisors array
+        }
+    }
+
+    return divisors.filter((el, ind, arr) => arr.indexOf(el) === ind)
+        .sort((a, b) => a - b); // Return the array of divisors
+};
+
+console.log(getComDivisirs(678, 357));
 
 // No. 4
 
