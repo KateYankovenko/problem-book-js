fffd734c4abe5b3be8da2c30cb65aad0de6470db
KateYankovenko <k.yankovenko@ukr.net>
2024-03-25 21:56:52 +0100

    3.7

--- level3.7.js
+++ level3.7.js
@@ -58,23 +58,106 @@ console.log(getComDivisirs(678, 357));
 
 // No. 4
 
+//--------------------------PRIME NUMBER CHECK IS HERE!!!!!!!!----------------------------------
+
 // A number is given. Check that this number has only one divisor other than itself and one.
+
+const checkDivisor = num => {
+    if (num <= 1) {
+        return false; // Numbers less than or equal to 1 are not prime
+    }
+
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) {
+            return false; // Number is divisible by a number other than 1 and itself
+        }
+    }
+
+    return true; // Number is only divisible by 1 and itself
+};
+
+console.log(checkDivisor(7));
 
 // No. 5
 
 // Numbers are written separated by commas. Get the maximum of these numbers.
 
+const getMaxNums = (...args) => { 
+    const max = Math.max(...args);
+    return max;
+};
+
+console.log(getMaxNums(2, 5, 67, 89, 1, 11));
+
 // No. 6
 
 // Given an array of numbers. After each single-digit number, insert another of the same number.
 
+const numInsert = (...args) => { 
+    const newArr = [];
+    
+    for (let i = 0; i < args.length; i++) {
+        const num = args[i];
+        
+        // Check if the number is a single digit
+        if (num >= 0 && num < 10) {
+            newArr.push(num);
+        }
+        newArr.push(num);
+    }
+
+    return newArr;
+};
+
+console.log(numInsert(12, 3, 45, 6, 67, 89, 5));
+
 // No. 7
 
-// Given a string. Remove all vowels from it.
+// Given a string. Remove all vowels from it.//A, E, I, O, U, and Y
+
+const removeVowels = str => { 
+    let newStr = "";
+    // console.log(str.);
+
+        const vowels = [
+            "a",
+            "e",
+            "i",
+            "o",
+            "u",
+            "A",
+            "E",
+            "I",
+            "O",
+            "U",
+        ]; 
+
+    str.split("").forEach((char) => {
+        if (!vowels.includes(char)) {
+            newStr += char;
+        }
+    });
+    
+    return newStr;
+};
+
+console.log(removeVowels("let me sing a song"));
 
 // No. 8
 
 // Given a string. Capitalize the last letter of each word on this line.
+
+const cptlzLast = str => {
+    let newStr = "";
+   
+    for (let i = 0; i < str.split(" ").length; i++){
+        
+    }
+
+    return newStr;
+};
+
+console.log(cptlzLast("Write a letter to your teacher!"));
 
 // No. 9
 
